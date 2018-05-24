71b4252ef0e24dbe9f598593c03690e5b4c71063
Saulo Carvalho <saulo.carv@hotmail.com>
2018-05-23 21:21:57 -0300

    Construção menu

--- Agenda/Program.cs
+++ Agenda/Program.cs
@@ -10,22 +10,38 @@ namespace Agenda
 
             Pessoa dadosPessoa = new Pessoa();
 
+            int opcao;
+
+            do
+            {
+                Console.WriteLine("Gerenciador de Aniversario");
+                Console.WriteLine("Selecione uma das opções abaixo");
+                Console.WriteLine("1 - Pesquisar Pessoas");
+                Console.WriteLine("2 - Adicionar Nova Pessoa");
+                Console.WriteLine("3 - Sair");
+                opcao = int.Parse(Console.ReadLine());
+
+                if (opcao == 2)
+                {
+                    Console.WriteLine("Nome :");
+                    dadosPessoa.Nome = Console.ReadLine();
+                    Console.WriteLine("Data de Nascimento");
+                    dadosPessoa.DataDeNascimento = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+
+                    dadosPessoa.AdicionarNome(dadosPessoa);
+                }
+                    
 
 
+            } while (opcao != 3);
 
-            Console.WriteLine("Nome :");
-               dadosPessoa.Nome = Console.ReadLine();
-            Console.WriteLine("Data de Nascimento");
-            dadosPessoa.DataDeNascimento = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
 
-            dadosPessoa.AdicionarNome(dadosPessoa);
 
 
             Console.ReadKey();
-
-
-
-
         }
+
+        
+
     }
 }
